2bbc0bd7d213e25a9ce176aa39840ed07d0431b6
wrectou <wrectou@gmail.com>
2021-03-02 14:42:46 +0800

    增加对props的限制

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { nanoid } from 'nanoid'
 import './index.css'
 
 export default class Header extends Component {
 
-  handleKeyUp = (e) => {
+
+  static propTypes = {      // 对接收的props进行类型和必要性的限制
+    addTodo: PropTypes.func.isRequired,
+  }
+
+  handleKeyUp = (e) => {      // 键盘事件的回调
     const { addTodo } = this.props
     const { keyCode, target } = e
     if (keyCode !== 13) return
