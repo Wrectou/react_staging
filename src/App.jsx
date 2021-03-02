d35aa78839242106cc4acf8921454e1e27ac9eab
wrectou <wrectou@gmail.com>
2021-03-02 14:06:58 +0800

    鼠标移入移出item处理，某一项todo状态的处理

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import Footer from './components/Footer'
 import './App.css'
 
 export default class App extends Component{
+  // 状态在哪里，操作状态的方法就在哪里！！！！！
 
   state = {
     todos: [
       {id: '001', name: '吃饭', done: true},
       {id: '002', name: '睡觉', done: true},
       {id: '003', name: '打豆豆', done: false},
-    ]
+    ],
   }
 
   addTodo = (todoObj) => {      // 用于添加一个新的todo对象
     const { todos } = this.state
     const newTodos = [todoObj, ...todos]
+    this.setState({ todos: newTodos })
+  }
+
+  updateTodo = (id, done) => {      // 用于更新一个todo对象
+    const { todos } = this.state
+    // 处理id相匹配的数据 / 状态更改的数据
+    const newTodos = todos.map((todoObj) => {
+      if (todoObj.id === id) return {...todoObj, done}
+      else return todoObj
+    })
     this.setState({ todos: newTodos })
   }
 
@@ -26,7 +37,7 @@ export default class App extends Component{
       <div className="todo-container">
         <div className="todo-wrap">
           <Header addTodo={this.addTodo} />
-          <List todos={todos} />
+          <List updateTodo={this.updateTodo} todos={todos} />
           <Footer/>
         </div>
       </div>
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,33 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+  state = { 
+    move: false,      // 鼠标移入移出状态
+  }
+
+  handleMouse = (flag) => {     // 鼠标移入移出回调
+    return () => {
+      this.setState({ move: flag })
+    }
+  }
+
+  handleCheck = (id) => {     // 完成 / 没完成某一项todo的回调
+    return (e) => {
+      this.props.updateTodo(id, e.target.checked)
+    }
+  }
+
   render() {
-    const { name, done } = this.props
+    const { id, name, done } = this.props
+    const { move } = this.state
     return (
-      <li>
+      <li style={{backgroundColor: move ? '#ddd' : '#fff'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:"none"}}>删除</button>
+        <button className="btn btn-danger" style={{display: move ? 'block' : 'none'}}>删除</button>
       </li>
     )
   }
