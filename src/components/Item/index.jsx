4b721b799978b260b8bb3c6b202ba4d0acc4cd34
wrectou <wrectou@gmail.com>
2021-03-02 10:08:50 +0800

    todoList 结构拆分+todos数据传递

---
+++ src/components/Item/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Item extends Component {
+  render() {
+    const { id, name, done } = this.props
+    console.log(this);
+    return (
+      <li>
+        <label>
+          <input type="checkbox" defaultChecked={done}/>
+          <span>{name}</span>
+        </label>
+        <button className="btn btn-danger" style={{display:"none"}}>删除</button>
+      </li>
+    )
+  }
+}
