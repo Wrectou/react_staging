6b63f69d0f5104e37a536fe03dba93f72bd7e688
wrectou <wrectou@gmail.com>
2021-03-07 17:09:26 +0800

    路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
-
+import { Link, Route } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Search />
-        <List />
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中，靠<a>跳转不同的页面 */}
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+
+              {/* 在react中依靠 路由链接 实现 切换组件  ----   编写路由链接 */}
+              <Link className="list-group-item" to="/about">About</Link>
+              <Link className="list-group-item" to="/home">Home</Link>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
