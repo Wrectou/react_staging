7939b39e54abc93843fc4f02a73f3333d59a8c12
wrectou <wrectou@gmail.com>
2021-03-11 09:37:48 +0800

    20_src_push与replace模式

---
+++ 20_src_push与replace模式/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+// import qs from 'querystring'
+
+const detailData = [
+  {id: '01', content: '哈哈哈，消息一'},
+  {id: '02', content: '这是消息二'},
+  {id: '03', content: 'React Router Params'},
+]
+
+export default class Detail extends Component {
+  render() {
+    console.log(this)
+    // 接收params参数
+    // const { id, title } = this.props.match.params
+
+    // 接收search参数
+    // const { search } = this.props.location
+    // const { id, title } = qs.parse(search.slice(1))
+
+    // 接收state参数
+    const { id, title } = this.props.location.state
+
+    const detail = detailData.find((item) => item.id === id)
+    return (
+      <ul>
+        <li>ID: {id}</li>
+        <li>TITLE: {title}</li>
+        <li>CONTENT: {detail.content}</li>
+      </ul>
+    )
+  }
+}
